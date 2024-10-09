932251608afd3d13939257812c0dcccd985383f4
luchosd02 <luchosilvadavidov@gmail.com>
2024-10-08 23:35:20 -0300

    mapa, crear, BD y sesion
    error en Crear Evento, pero el resto esta bien

--- App-Web/website/models.py
+++ App-Web/website/models.py
@@ -2,17 +2,37 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    puntos_interes = db.relationship('PuntoDeInteres')
+    eventos = db.relationship('Evento')
+
+class PuntoDeInteres(db.Model):
+    id = db.Column(db.Integer, primary_key=True,nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    nombre = db.Column(db.String(100), nullable=False)
+    descripcion = db.Column(db.Text, nullable=True)
+    latitud = db.Column(db.Float, nullable=False)
+    longitud = db.Column(db.Float, nullable=False)
+    imagenes = db.Column(db.String(100), nullable=True)
+    fecha_creacion = db.Column(db.DateTime(timezone=True), default=func.now())
+    tipo = db.Column(db.String(100), nullable=False)
+    apertura = db.Column(db.String(100), nullable=True)
+    cierre = db.Column(db.String(100), nullable=True)
+    
+class Evento(db.Model):
+    id = db.Column(db.Integer, primary_key=True,nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    nombre = db.Column(db.String(100), nullable=False)
+    descripcion = db.Column(db.Text, nullable=True)
+    latitud = db.Column(db.Float, nullable=False)
+    longitud = db.Column(db.Float, nullable=False)
+    imagenes = db.Column(db.String(100), nullable=True)
+    fecha_creacion = db.Column(db.DateTime(timezone=True), default=func.now())
+    tipo = db.Column(db.String(100), nullable=False)
+    fecha_inicio = db.Column(db.DateTime(timezone=True), nullable=False)
+    hora_inicio = db.Column(db.String(100), nullable=False)
+    hora_fin = db.Column(db.String(100), nullable=False)
--- App-Web/website/views.py
+++ App-Web/website/views.py
@@ -1,37 +1,175 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+# views.py
+from flask import Blueprint, redirect, render_template, request, flash, jsonify, url_for
 from flask_login import login_required, current_user
-from .models import Note
+from .models import PuntoDeInteres, Evento
 from . import db
 import json
 
 views = Blueprint('views', __name__)
 
-
 @views.route('/', methods=['GET', 'POST'])
 @login_required
-def home():
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
+def mapa():
+    # Obtener todos los puntos de interés
+    puntos = PuntoDeInteres.query.all()
+    puntos_geojson = []
+    for punto in puntos:
+        puntos_geojson.append({
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [punto.longitud,punto.latitud]
+            },
+            'properties': {
+                'nombre': punto.nombre,
+                'descripcion': punto.descripcion,
+                'tipo': 'punto_de_interes',
+                'imagenes': punto.imagenes,
+                'apertura': punto.apertura,
+                'cierre': punto.cierre,
+                'fecha_creacion': punto.fecha_creacion.isoformat()
+            }
+        })
 
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
-            db.session.commit()
-            flash('Note added!', category='success')
+    # Obtener todos los eventos
+    eventos = Evento.query.all()
+    eventos_geojson = []
+    for evento in eventos:
+        eventos_geojson.append({
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [evento.longitud,evento.latitud]
+            },
+            'properties': {
+                'nombre': evento.nombre,
+                'descripcion': evento.descripcion,
+                'tipo': 'evento',
+                'imagenes': evento.imagenes,
+                'fecha_inicio': evento.fecha_inicio.isoformat(),
+                'duracion': evento.duracion,
+                'fecha_creacion': evento.fecha_creacion.isoformat()
+            }
+        })
 
-    return render_template("home.html", user=current_user)
+    # Crear un FeatureCollection combinando los puntos de interés y los eventos
+    mapa_data = {
+        'type': 'FeatureCollection',
+        'features': puntos_geojson + eventos_geojson
+    }
 
+    # Renderizar el template y pasar los datos al frontend
+    return render_template("mapa.html", user=current_user, mapa_data=json.dumps(mapa_data))
 
-@views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
+@views.route('/api/puntos_interes')
+def get_puntos_interes():
+    puntos = PuntoDeInteres.query.all()
+    puntos_geojson = []
+    for punto in puntos:
+        puntos_geojson.append({
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [punto.longitud,punto.latitud]
+            },
+            'properties': {
+                'nombre': punto.nombre,
+                'descripcion': punto.descripcion,
+                'tipo': 'punto_de_interes',
+                'imagenes': punto.imagenes,
+                'apertura': punto.apertura,
+                'cierre': punto.cierre,
+                'fecha_creacion': punto.fecha_creacion.isoformat()
+            }
+        })
+    return jsonify({
+        'type': 'FeatureCollection',
+        'features': puntos_geojson
+    })
 
-    return jsonify({})
+@views.route('/api/eventos')
+def get_eventos():
+    eventos = Evento.query.all()
+    eventos_geojson = []
+    for evento in eventos:
+        eventos_geojson.append({
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [evento.longitud,evento.latitud]
+            },
+            'properties': {
+                'nombre': evento.nombre,
+                'descripcion': evento.descripcion,
+                'tipo': evento.tipo,
+                'imagenes': evento.imagenes,
+                'fecha_inicio': evento.fecha_inicio.isoformat(),
+                'hora_incio': evento.hora_inicio.isoformat(),
+                'hora_fin': evento.hora_fin.isoformat(),
+                'fecha_creacion': evento.fecha_creacion.isoformat()
+            }
+        })
+    return jsonify({
+        'type': 'FeatureCollection',
+        'features': eventos_geojson
+    })
+    
+@views.route('/nuevo_punto', methods=['GET', 'POST'])
+def nuevo_punto():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        descripcion = request.form['descripcion']
+        latitud = request.form['latitud']
+        longitud = request.form['longitud']
+        tipo = request.form['tipo']
+        apertura = request.form['apertura']
+        cierre = request.form['cierre']
+        
+        nuevo_punto = PuntoDeInteres(
+            nombre=nombre,
+            descripcion=descripcion,
+            latitud=latitud,
+            longitud=longitud,
+            tipo=tipo,
+            apertura=apertura,
+            cierre=cierre,
+            user_id=current_user.id
+        )
+        
+        db.session.add(nuevo_punto)
+        db.session.commit()
+        flash('Punto de Interés creado con éxito.')
+        return redirect(url_for('views.nuevo_punto'))
+
+    return render_template('nuevo_punto.html')
+
+@views.route('/nuevo_evento', methods=['GET', 'POST'])
+def nuevo_evento():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        descripcion = request.form['descripcion']
+        latitud = request.form['latitud']
+        longitud = request.form['longitud']
+        tipo = request.form['tipo']
+        fecha_inicio = request.form['fecha_inicio']
+        hora_inicio = request.form['hora_inicio']
+        hora_fin = request.form['hora_fin']
+        
+        nuevo_evento = Evento(
+            nombre=nombre,
+            descripcion=descripcion,
+            latitud=latitud,
+            longitud=longitud,
+            tipo=tipo,
+            fecha_inicio=fecha_inicio,
+            hora_inicio=hora_inicio,
+            hora_fin=hora_fin,
+            user_id=current_user.id
+        )
+        
+        db.session.add(nuevo_evento)
+        db.session.commit()
+        flash('Evento creado con éxito.')
+        return redirect(url_for('views.nuevo_evento'))
+
+    return render_template('nuevo_evento.html')
